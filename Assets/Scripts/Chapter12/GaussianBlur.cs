2af33bfa5c3e274f176539b97356a602e7980b77
Richbabe <362278478@qq.com>
2018-10-31 22:44:36 +0800

    学习基于屏幕后处理的边缘检测和高斯模糊

---
+++ Assets/Scripts/Chapter12/GaussianBlur.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaussianBlur : PostEffectsBase{
+    public Shader gaussianBlurShader;
+    private Material gaussianBlurMaterial = null;
+
+    public Material material
+    {
+        get
+        {
+            gaussianBlurMaterial = CheckShaderAndCreateMaterial(gaussianBlurShader, gaussianBlurMaterial);
+            return gaussianBlurMaterial;
+        }
+    }
+
+    [Range(0, 4)]
+    public int iterations = 3;//高斯模糊迭代次数
+
+    [Range(0.2f, 3.0f)]
+    public float blurSpread = 0.6f;//模糊范围
+
+    [Range(1, 8)]
+    public int downSample = 2;//缩放系数
+
+    /// 1st edition: just apply blur
+    //	void OnRenderImage(RenderTexture src, RenderTexture dest) {
+    //		if (material != null) {
+    //			int rtW = src.width;
+    //			int rtH = src.height;
+    //			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+    //
+    //			// Render the vertical pass
+    //			Graphics.Blit(src, buffer, material, 0);
+    //			// Render the horizontal pass
+    //			Graphics.Blit(buffer, dest, material, 1);
+    //
+    //			RenderTexture.ReleaseTemporary(buffer);
+    //		} else {
+    //			Graphics.Blit(src, dest);
+    //		}
+    //	} 
+
+    /// 2nd edition: scale the render texture
+    //	void OnRenderImage (RenderTexture src, RenderTexture dest) {
+    //		if (material != null) {
+    //			int rtW = src.width/downSample;
+    //			int rtH = src.height/downSample;
+    //			RenderTexture buffer = RenderTexture.GetTemporary(rtW, rtH, 0);
+    //			buffer.filterMode = FilterMode.Bilinear;
+    //
+    //			// Render the vertical pass
+    //			Graphics.Blit(src, buffer, material, 0);
+    //			// Render the horizontal pass
+    //			Graphics.Blit(buffer, dest, material, 1);
+    //
+    //			RenderTexture.ReleaseTemporary(buffer);
+    //		} else {
+    //			Graphics.Blit(src, dest);
+    //		}
+    //	}
+
+    /// 3rd edition: use iterations for larger blur
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            int rtW = src.width / downSample;
+            int rtH = src.height / downSample;
+
+            RenderTexture buffer0 = RenderTexture.GetTemporary(rtW, rtH, 0);
+            buffer0.filterMode = FilterMode.Bilinear;
+
+            Graphics.Blit(src, buffer0);
+
+            for (int i = 0; i < iterations; i++)
+            {
+                material.SetFloat("_BlurSize", 1.0f + i * blurSpread);
+
+                RenderTexture buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                // Render the vertical pass
+                Graphics.Blit(buffer0, buffer1, material, 0);
+
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+                buffer1 = RenderTexture.GetTemporary(rtW, rtH, 0);
+
+                // Render the horizontal pass
+                Graphics.Blit(buffer0, buffer1, material, 1);
+
+                RenderTexture.ReleaseTemporary(buffer0);
+                buffer0 = buffer1;
+            }
+
+            Graphics.Blit(buffer0, dest);
+            RenderTexture.ReleaseTemporary(buffer0);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/Shaders/Chapter12/Chapter12-GaussianBlur.shader
@@ -0,0 +1,94 @@
+﻿Shader "Unity Shaders Book/Chapter 12/Chapter12-GaussianBlur" {
+	Properties {
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_BlurSize ("Blur Size", Float) = 1.0
+	}
+	SubShader {
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		//定义Properties属性
+		sampler2D _MainTex;
+		half4 _MainTex_TexelSize;
+		float _BlurSize;
+
+		struct v2f {
+			float4 pos : SV_POSITION;
+			half2 uv[5] : TEXCOORD0;
+		};
+
+		//计算垂直方向坐标
+		v2f vertBlurVertical(appdata_img v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+
+			half2 uv = v.texcoord;
+
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[2] = uv - float2(0.0, _MainTex_TexelSize.y * 1.0) * _BlurSize;
+			o.uv[3] = uv + float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+			o.uv[4] = uv - float2(0.0, _MainTex_TexelSize.y * 2.0) * _BlurSize;
+
+			return o;
+		}
+
+		//计算水平方向坐标
+		v2f vertBlurHorizontal(appdata_img v) {
+			v2f o;
+			o.pos = UnityObjectToClipPos(v.vertex);
+
+			half2 uv = v.texcoord;
+
+			o.uv[0] = uv;
+			o.uv[1] = uv + float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[2] = uv - float2(_MainTex_TexelSize.x * 1.0, 0.0) * _BlurSize;
+			o.uv[3] = uv + float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+			o.uv[4] = uv - float2(_MainTex_TexelSize.x * 2.0, 0.0) * _BlurSize;
+
+			return o;
+		}
+
+		//片段着色器
+		fixed4 fragBlur(v2f i) : SV_Target{
+			float weight[3] = { 0.4026, 0.2442, 0.0545 };//由于对称性，只用保存三个高斯权重
+
+			fixed3 sum = tex2D(_MainTex, i.uv[0]).rgb * weight[0];
+
+			for (int it = 1; it < 3; it++) {
+				sum += tex2D(_MainTex, i.uv[it * 2 - 1]).rgb * weight[it];
+				sum += tex2D(_MainTex, i.uv[it * 2]).rgb * weight[it];
+			}
+
+			return fixed4(sum, 1.0);
+		}
+
+		ENDCG
+
+		ZTest Always Cull Off ZWrite Off
+
+		Pass {
+			NAME "GAUSSIAN_BLUR_VERTICAL"
+
+			CGPROGRAM
+
+			#pragma vertex vertBlurVertical  
+			#pragma fragment fragBlur
+
+			ENDCG
+		}
+
+		Pass {
+			NAME "GAUSSIAN_BLUR_HORIZONTAL"
+
+			CGPROGRAM
+
+			#pragma vertex vertBlurHorizontal  
+			#pragma fragment fragBlur
+
+			ENDCG
+		}
+	}
+	FallBack Off
+}
